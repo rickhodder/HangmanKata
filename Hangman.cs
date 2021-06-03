cb9d44a2c02b78e3a3b518de9ddd594e4b87f2b3
rickhodder <rickhodder@msn.com>
2021-06-02 21:48:15 -0400

    InProgress property

--- Hangman.cs
+++ Hangman.cs
@@ -4,12 +4,12 @@ namespace HangmanKata
     {
         private readonly string _secretWord;
         private readonly int _incorrectGuesses;
+        public bool InProgress { get; set; } = true;
 
         public Hangman(string secretWord, int incorrectGuesses)
         {
             _secretWord = secretWord.ToUpper();
             _incorrectGuesses = incorrectGuesses;
         }
-
     }
 }
--- HangmanTests.cs
+++ HangmanTests.cs
@@ -6,9 +6,10 @@ namespace HangmanKata
     public class HangmanTests
     {
         [Fact]
-        public void Ctor_Settings()
+        public void Ctor_InProgress()
         {
             var sut = new Hangman("allcaps", 3);
+            Assert.True(sut.InProgress);
         }
     }
 }
